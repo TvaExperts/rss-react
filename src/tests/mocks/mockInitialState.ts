9b13e365cb5380943ce5671ea80bf29e48cce662
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-11-12 23:00:16 +0300

    fix: fix state property

--- src/tests/mocks/mockInitialState.ts
+++ src/tests/mocks/mockInitialState.ts
@@ -1,11 +1,11 @@
 import { AppState } from '../../reducers/appReducer';
 import { mockArrOf10Products } from './mockArrOf10Products';
-import { DEFAULT_LIMIT, DEFAULT_OFFSET } from '../../constants/searchParams';
+import { DEFAULT_LIMIT } from '../../constants/searchParams';
 
 export const initialState: AppState = {
   total: 10,
   products: mockArrOf10Products,
   query: '',
   limit: DEFAULT_LIMIT,
-  offset: DEFAULT_OFFSET,
+  page: 1,
 };
