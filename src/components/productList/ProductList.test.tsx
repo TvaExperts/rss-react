6c6f950e8ae68bcbfc5e58b39747dfb333d342a5
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-11-20 00:37:35 +0300

    fix: fix икщbroken test

--- src/components/productList/ProductList.test.tsx
+++ src/components/productList/ProductList.test.tsx
@@ -1,41 +1,32 @@
+import { vi } from 'vitest';
 import { renderWithRouterAndRedux } from '../../tests/helpers/renderWithRouterAndRedux';
 import ProductList from './ProductList';
 import { mockArrOf30Products } from '../../tests/mocks/mockArrOf30Products';
-import { renderWithRedux } from '../../tests/helpers/renderWithRedux';
+import * as api from '../../services/api';
 
 describe('Product List tests', () => {
   it('Component renders the specified number of cards', async () => {
-    const initialState = {
-      productsReducer: {
-        total: mockArrOf30Products.length,
-        products: mockArrOf30Products,
-        isLoading: false,
-        error: '',
-      },
-      appSearchParamsReducer: {
-        limit: 10,
-        page: 1,
-        query: '',
-      },
-    };
-    const { findAllByRole } = renderWithRouterAndRedux(<ProductList />, {
-      initialState,
-    });
+    const { findAllByRole } = renderWithRouterAndRedux(<ProductList />);
     const listItems = await findAllByRole('listitem');
     expect(listItems.length).toBe(mockArrOf30Products.length);
   });
 
-  it('Component renders warning message if no cards are present.', () => {
-    const initialState = {
-      productsReducer: {
-        total: 0,
-        products: [],
-        isLoading: false,
-        error: '',
-      },
-    };
-    const { getByText } = renderWithRedux(<ProductList />, initialState);
-    const warningMessageElement = getByText(
+  it('Component renders warning message if no cards are present.', async () => {
+    vi.spyOn(api, 'useGetSearchProductsOnPageQuery').mockImplementation(
+      vi.fn().mockImplementation(() => {
+        return {
+          isError: false,
+          isFetching: false,
+          data: {
+            total: 0,
+            products: [],
+          },
+        };
+      })
+    );
+
+    const { findByText } = renderWithRouterAndRedux(<ProductList />);
+    const warningMessageElement = await findByText(
       /Nothing was found, make another request/i
     );
     expect(warningMessageElement).toBeInTheDocument();
