a5c4a4ac6457674c4942d03c07d8d0453991eeaa
Trachenko Viktor <trachenko.v.a@gmail.com>
2023-12-04 02:20:28 +0300

    fix: fix react hook form country input

--- src/components/forms/ReactHookForm.tsx
+++ src/components/forms/ReactHookForm.tsx
@@ -1,32 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from 'react-router-dom';
 import styles from './Form.module.css';
 import { FormDataInputs } from '../../types';
 import schema from './resolvers/schema';
-import { useAppDispatch } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { formsDataActions } from '../../reducers/FormsDataSlice';
 import ROUTES from '../../router/routes';
 import { convertInputsDataToStore } from '../../utils/convertInputsDataToStore';
+import { filterCountries } from '../../utils/filterCountries';
 
 function ReactHookForm() {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+
+  const [isOpenSuggestion, setIsOpenSuggestion] = useState<boolean>(false);
+
+  const { countries } = useAppSelector((state) => state.countriesReducer);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    getValues,
+    setValue,
+    watch,
+    trigger,
   } = useForm<FormDataInputs>({
     mode: 'onChange',
     resolver: yupResolver<FormDataInputs>(schema),
   });
+
+  const [filteredCountries, setFilteredCountries] = useState<string[]>([]);
 
   const onSubmit: SubmitHandler<FormDataInputs> = async (formData) => {
     const dataToStore = await convertInputsDataToStore(formData);
     dispatch(formsDataActions.addLine(dataToStore));
     navigate(ROUTES.home);
   };
+
+  const countryText = watch('country', '');
+
+  useEffect(() => {
+    setValue('country', countryText);
+    const suggestions = filterCountries(countries, countryText);
+    setFilteredCountries(suggestions);
+    if (!(suggestions.length === 1 && suggestions[0] === countryText)) {
+      setIsOpenSuggestion(true);
+    } else {
+      setIsOpenSuggestion(false);
+    }
+  }, [countries, countryText, setValue]);
+
+  function handleClickSuggestion(country: string) {
+    setValue('country', country);
+    setFilteredCountries(filterCountries(countries, country));
+    setIsOpenSuggestion(false);
+    trigger('country');
+  }
+
+  function hasSuggestions() {
+    return (
+      getValues().country &&
+      getValues().country.length !== 0 &&
+      filteredCountries.length !== 0 &&
+      isOpenSuggestion
+    );
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
@@ -100,19 +141,30 @@ function ReactHookForm() {
           <p className={styles.errorMessage}>{errors.imageFile.message}</p>
         )}
       </label>
-      <label htmlFor="country">
+
+      <label htmlFor="country" className={styles.label}>
         Country:
-        <input
-          {...register('country')}
-          type="text"
-          placeholder="Select your country..."
-          id="country"
-          name="country"
-        />
+        <input type="text" id="country" {...register('country')} />
         {errors.country && (
           <p className={styles.errorMessage}>{errors.country.message}</p>
         )}
+        {hasSuggestions() && (
+          <ul className={styles.list}>
+            {filteredCountries.map((country) => {
+              return (
+                <li
+                  className={styles.item}
+                  key={country}
+                  onClick={() => handleClickSuggestion(country)}
+                >
+                  {country}
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </label>
+
       <label htmlFor="acceptTC">
         Accept T&C:
         <input
